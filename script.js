81f5cba7619420c63e62d2887a390d101f81bf8c
kevinyuanzy <yuansirzy126@gmail.com>
2025-02-10 22:18:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,35 @@ const map = new mapboxgl.Map({
     container: 'Lab2Map', // map container ID in the index.html file.
     style: 'mapbox://styles/kevinyuanzy/cm6ztbpqc003s01qwcvdrf6ft', // style URL from created MapBox style.
     center: [-79.391820, 43.660168], // starting position [lng, lat]. I used the starting location centered approximately at Queen's Park, Toronto. 
-    zoom: 12.5, // starting zoom level. I use the zoom level that can display the whole Downtown Toronto.
+    zoom: 13.5, // starting zoom level. I use the zoom level that can display the whole U of T campus.
 });
 
-// Add a data source from a GeoJSON file
-map.addSource('ParksAroundUofT', {
-    type: 'geojson',
-    data: 'https://raw.githubusercontent.com/kevinyuanzy/472-Lab2-KY/refs/heads/main/ParksAroundUofT.geojson' // Your URL to your buildings.geojson file
+//Add "on load" event listener
+map.on('load', () => {
+    //Add the .geojson file from GitHub repository
+    map.addSource('ParksAroundUofT', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/kevinyuanzy/472-Lab2-KY/refs/heads/main/ParksAroundUofT.geojson' // The URL to my GeoJson polygon.
+    });
+    
+    // Add layer style to the map to represent polygons
+    map.addLayer({
+        'id': 'parks-polygons',
+        'type': 'fill',
+        'source': 'ParksAroundUofT',
+        'paint': {
+            'fill-color': '#00b33c', 
+            'fill-opacity': 0.4,
+            'fill-outline-color': 'black'
+        }
+    });
+
+
+    
+    
+
+
 });
 
-map.addLayer({
-    'id': 'parks-polygons',
-    'type': 'fill',
-    'source': 'ParksAroundUofT',
-    'paint': {
-        'circle-radius': 5,
-        'circle-color': '#007cbf'
-    }
-});
+
+
